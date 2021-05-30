9bdfe3ef199ccb62ee1e4100d349fcdc3b43900b
KotletJB <bronowski.jan2@gmail.com>
2021-05-30 23:04:13 +0200

    starting templates

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World !!!'
+def home():
+    return render_template("page1.html", content="base")
+
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
 
 
 if __name__ == '__main__':
